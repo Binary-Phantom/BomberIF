02525d7a5da1654174274b0889b34c90bab5d9ee
Binary-Phantom <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>
2025-12-11 17:38:26 -0300

    fix: solved the bug causing react versions mismatch "TS2786"

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -11,4 +11,4 @@ export const SERVER_URL = ONDEV
   ? 'http://192.168.2.81:4000'
   : 'http://192.168.2.81:4000'
  
-//export const SERVER_URL = ONDEV ? 'http://192.168.2.81:4000' /*: 'https://bomberif-server.onrender.com'*/:'http://localhost:4000'
+// export const SERVER_URL = ONDEV ? 'http://192.168.2.81:4000' /*: 'https://bomberif-server.onrender.com'*/:'http://localhost:4000'
