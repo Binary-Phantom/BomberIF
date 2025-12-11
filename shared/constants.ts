4dc94030f79469bf6ec64f5252a4f31b2dd75cda
Binary-Phantom <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>
2025-12-11 01:25:40 -0300

    fix: import wss certificate + minor error fixes

---
+++ shared/constants.ts
@@ -0,0 +1,40 @@
+export const BLASTS = 1
+
+export const BOMB_MOVE = 3 // moves 2 tiles when kicked
+
+export const BOMBS = 8
+
+export const BOMB_SPEED = 2
+
+export const BONUS = 5
+
+export const BONUSES = [
+  0,  // none
+  8,  // bomb
+  16, // blast
+  2,  // holding
+  2,  // kick
+  8,  // speed
+  2,  // slow
+  2,  // pass
+  2,  // invert
+  2   // kill
+]
+
+export const ID_LENGTH = 4
+
+export const INITIAL_POSITION = [[17,9], [209,9], [17,169], [209,169]]
+
+export const MAX_SPEED = 1.4
+
+export const NICK = { MIN: 2, MAX: 12 }
+
+export const PRESS_INTERVAL = 150
+
+export const SPEED = 1
+
+export const SPRITES = 28
+
+export const STAGES = 28
+
+export const TILE_SIZE = 16
