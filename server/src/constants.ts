250ab19508881047b8dc4421c491378f34fbc090
Binary-Phantom <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>
2025-12-27 03:47:19 -0300

    fix: render traceback error

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,21 +1,17 @@
-const ORIGINS:Record<string,boolean> = {
+const ORIGINS: Record<string, boolean> = {
   'http://localhost:3000': true,
   'https://localhost:3000': true,
   'http://192.168.1.5:3000': true,
   'https://192.168.1.5:3000': true,
-  'http://binary-phantom.github.io/BomberIF': true,
-  'https://binary-phantom.github.io/BomberIF': true,
   'https://binary-phantom.github.io': true
 }
 
 export const CORS = {
   methods: ['GET'],
-  origin: (origin:string|undefined, callback:(error:Error|null,allow?:boolean) => void) => {
-    if (ORIGINS[origin as string]) callback(null, true)
-    else                           callback(null, false)
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    // Socket.IO às vezes manda origin undefined (SSR, healthcheck)
+    if (!origin) return callback(null, true)
+
+    callback(null, !!ORIGINS[origin])
   }
 }
-
-export const MAX_PLAYERS = 4
-
-export const PORT = process.env.PORT || 4000
